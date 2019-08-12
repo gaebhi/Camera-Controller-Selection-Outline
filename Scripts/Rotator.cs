3f43104174210dddcbfe5c74f9f0dfa63957cedb
gaebhi <kdkim89@gmail.com>
2019-08-12 16:04:56 +0900

    selection outline shader 구현 / 각진 오브젝트들의 노말을 수정하여 아웃라인이 끊기지 않도록 하는 기능 구현

---
+++ Scripts/Rotator.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rotator : MonoBehaviour
+{
+    public GameObject[] ObjectList;
+
+    private void Update()
+    {
+        foreach (var _obj in ObjectList)
+        {
+            _obj.transform.Rotate(Vector3.up * Time.deltaTime * 25f);
+        }
+    }
+}
